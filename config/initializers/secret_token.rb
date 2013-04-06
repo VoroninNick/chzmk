8a7e80cb61bee4441544c20f3672bb77e7cf5f64
Vik Ewoods <support@voroninstudio.eu>
2013-04-06 15:02:42 +0300

    Initial commit
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CHZMK::Application.config.secret_token = '7a5bf4c64a6961e56a75d5b2fbb494adeacb14409e655074549a0fbd4c0c40e0f5491298e4e1ebcd21b71f295bf3203ba3884da10b6cec540d7f11cc85870edb'
